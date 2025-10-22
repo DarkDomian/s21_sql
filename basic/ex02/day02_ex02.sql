79e47b7b3f18774c440b7c0b5e56a0ef41754883
Demian trelawnm Domozhirov <darkdomian@gmail.com>
2025-10-22 20:38:33 +0300

    exersice(sql): finish task by creating 10 sql query

---
+++ basic/ex02/day02_ex02.sql
@@ -0,0 +1,11 @@
+-- using comparison signs
+SELECT name, rating
+FROM pizzeria
+WHERE rating >= 3.5 AND rating <= 5
+ORDER BY rating;
+
+-- using key word
+SELECT name, rating
+FROM pizzeria
+WHERE rating BETWEEN 3.5 AND 5
+ORDER BY rating;
---
+++ basic/ex08/day00_ex08.sql
@@ -0,0 +1,4 @@
+SELECT DISTINCT *
+FROM person_order
+WHERE id % 2 = 0
+ORDER BY id ASC;
