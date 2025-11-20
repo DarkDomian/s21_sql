81fbb5e767247172e4a936c433e84e3a897fbd3a
Selifanchik <selifanchik@mail.ru>
2025-11-20 21:24:32 +0300

    day05: update task1

--- salesman-problem/ex00/team00_ex00.sql
+++ salesman-problem/ex00/team00_ex00.sql
@@ -1,55 +1,61 @@
-drop table city;
+DROP TABLE IF EXISTS hamiltonian_cycle;
 
-create table city (
-    id bigint PRIMARY KEY,
-    point1 varchar not null,
-    point2 varchar not null,
-    cost integer not null
+CREATE TABLE hamiltonian_cycle (
+    point1 VARCHAR NOT NULL,
+    point2 VARCHAR NOT NULL,
+    cost INTEGER NOT NULL
 );
 
-INSERT into city VALUES (1, 'a', 'b', 10);
-INSERT into city VALUES (2, 'a', 'c', 15);
-INSERT into city VALUES (3, 'a', 'd', 20);
-INSERT into city VALUES (4, 'b', 'a', 10);
-INSERT into city VALUES (5, 'b', 'c', 35);
-INSERT into city VALUES (6, 'b', 'd', 25);
-INSERT into city VALUES (7, 'c', 'a', 15);
-INSERT into city VALUES (8, 'c', 'b', 35);
-INSERT into city VALUES (9, 'c', 'd', 30);
-INSERT into city VALUES (10, 'd', 'a', 20);
-INSERT into city VALUES (11, 'd', 'b', 25);
-INSERT into city VALUES (12, 'd', 'c', 30);
+INSERT into hamiltonian_cycle VALUES 
+('a', 'b', 10),
+('a', 'c', 15),
+('a', 'd', 20),
+('b', 'a', 10),
+('b', 'c', 35),
+('b', 'd', 25),
+('c', 'a', 15),
+('c', 'b', 35),
+('c', 'd', 30),
+('d', 'a', 20),
+('d', 'b', 25),
+('d', 'c', 30);
 
-WITH RECURSIVE paths as (
-    SELECT 
-        point1 as start,
-        point2 as current,
-        cost as total_cost,
-        array[point1, point2] as tour
-    FROM city
-    WHERE point1 = 'a'
-    UNION ALL
-    SELECT 
-        p.start as start,
-        c.point2 as current,
-        p.total_cost + c.cost as total_cost,
-        array_append(tour, c.point2) 
-    FROM paths p
-    JOIN city c ON p.current = c.point1
-    WHERE 
-        NOT (c.point2 = ANY(tour)) 
-        OR (
-            array_length(tour, 1) = (SELECT COUNT(DISTINCT point1) FROM city) 
-            AND c.point2 = 'a'
-        )
-),
-total_paths as (
-    SELECT 
-        total_cost,
-        tour
-    FROM paths
-    WHERE 
-        array_length(tour, 1) = (SELECT COUNT(DISTINCT point1) FROM city) + 1
-)
-WHERE total_cost = (SELECT MIN(total_cost) FROM total_paths)
+WITH RECURSIVE
+    paths AS (
+        SELECT 
+            point2 AS current,
+            cost AS total_cost,
+            array[point1, point2] AS tour
+        FROM hamiltonian_cycle
+        WHERE point1 = 'a'
+        UNION ALL
+        SELECT
+            c.point2 AS current,
+            p.total_cost + c.cost AS total_cost,
+            array_append(tour, c.point2) 
+        FROM paths p
+        INNER JOIN hamiltonian_cycle c ON 
+            p.current = c.point1
+        WHERE 
+            NOT (c.point2 = ANY(tour))
+            OR (
+                array_length(tour, 1) = (
+                    SELECT COUNT(DISTINCT point1) 
+                    FROM hamiltonian_cycle
+                )
+                AND c.point2 = 'a'
+            )
+    ),
+    full_paths AS (
+        SELECT *
+        FROM paths
+        WHERE 
+            array_length(tour, 1) = (
+                SELECT COUNT(DISTINCT point1) 
+                FROM hamiltonian_cycle
+            ) + 1
+    )
+SELECT *
+FROM full_paths
+WHERE total_cost = (SELECT MIN(total_cost) FROM full_paths)
 ORDER BY total_cost, tour;
