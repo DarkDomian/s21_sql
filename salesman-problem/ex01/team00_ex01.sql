ccf79bde66eadbe8102acc675bac177d58f1c6ff
Selifanchik <selifanchik@mail.ru>
2025-11-20 21:24:56 +0300

    day05: update task2

--- salesman-problem/ex01/team00_ex01.sql
+++ salesman-problem/ex01/team00_ex01.sql
@@ -1,37 +1,41 @@
-WITH RECURSIVE paths as (
-    SELECT 
-        point1 as start,
-        point2 as current,
-        cost as total_cost,
-        array[point1, point2] as tour
-    FROM city
-    WHERE point1 = 'a'
-    UNION ALL
-    SELECT 
-        p.start as start,
-        c.point2 as current,
-        p.total_cost + c.cost as total_cost,
-        array_append(tour, c.point2) 
-    FROM paths p
-    JOIN city c ON p.current = c.point1
-    WHERE 
-        NOT (c.point2 = ANY(tour)) 
-        OR (
-            array_length(tour, 1) = (SELECT COUNT(DISTINCT point1) FROM city) 
-            AND c.point2 = 'a'
-        )
-),
-total_paths as (
-    SELECT 
-        total_cost,
-        tour
-    FROM paths
-    WHERE 
-        array_length(tour, 1) = (SELECT COUNT(DISTINCT point1) FROM city) + 1
-)
+WITH RECURSIVE
+    paths AS (
+        SELECT 
+            point2 AS current,
+            cost AS total_cost,
+            array[point1, point2] AS tour
+        FROM hamiltonian_cycle
+        WHERE point1 = 'a'
+        UNION ALL
+        SELECT
+            c.point2 AS current,
+            p.total_cost + c.cost AS total_cost,
+            array_append(tour, c.point2) 
+        FROM paths p
+        INNER JOIN hamiltonian_cycle c ON 
+            p.current = c.point1
+        WHERE 
+            NOT (c.point2 = ANY(tour))
+            OR (
+                array_length(tour, 1) = (
+                    SELECT COUNT(DISTINCT point1) 
+                    FROM hamiltonian_cycle
+                )
+                AND c.point2 = 'a'
+            )
+    ),
+    full_paths AS (
+        SELECT *
+        FROM paths
+        WHERE 
+            array_length(tour, 1) = (
+                SELECT COUNT(DISTINCT point1) 
+                FROM hamiltonian_cycle
+            ) + 1
+    )
 SELECT *
-FROM total_paths
+FROM full_paths
 WHERE 
-    total_cost = (SELECT MIN(total_cost) FROM total_paths) 
-    OR total_cost = (SELECT MAX(total_cost) FROM total_paths)
+	total_cost = (SELECT MIN(total_cost) FROM full_paths)
+    OR total_cost = (SELECT MAX(total_cost) FROM full_paths)
 ORDER BY total_cost, tour;
