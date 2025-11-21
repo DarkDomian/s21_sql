6af61dbacd6f8ecf355738e05ddc606dba78c697
Selifanchik <selifanchik@mail.ru>
2025-11-21 10:49:42 +0300

    (day05): update task2 - use view

--- salesman-problem/ex01/team00_ex01.sql
+++ salesman-problem/ex01/team00_ex01.sql
@@ -1,42 +1,8 @@
-WITH RECURSIVE
-    paths AS (
-        SELECT 
-            point2 AS current,
-            cost AS total_cost,
-            array[point1, point2] AS tour
-        FROM hamiltonian_cycle
-        WHERE point1 = 'a'
-        UNION ALL
-        SELECT
-            c.point2 AS current,
-            p.total_cost + c.cost AS total_cost,
-            array_append(tour, c.point2) 
-        FROM paths p
-        INNER JOIN hamiltonian_cycle c ON 
-            p.current = c.point1
-            AND (
-                NOT (c.point2 = ANY(tour))
-                OR (
-                    array_length(tour, 1) = (
-                        SELECT COUNT(DISTINCT point1) 
-                        FROM hamiltonian_cycle
-                    )
-                    AND c.point2 = 'a'
-                )
-            )
-    ),
-    full_paths AS (
-        SELECT *
-        FROM paths
-        WHERE 
-            array_length(tour, 1) > (
-                SELECT COUNT(DISTINCT point1) 
-                FROM hamiltonian_cycle
-            )
-    )
 SELECT *
-FROM full_paths
+FROM full_way
 WHERE 
-	total_cost = (SELECT MIN(total_cost) FROM full_paths)
-    OR total_cost = (SELECT MAX(total_cost) FROM full_paths)
+    total_cost = (SELECT MIN(total_cost) FROM full_way)
+    OR total_cost = (SELECT MAX(total_cost) FROM full_way)
 ORDER BY total_cost, tour;
+
+DROP VIEW IF EXISTS full_way;
