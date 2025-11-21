3e93039331030ab98aae9ee299606fe0d9673d59
Demian trelawnm Domozhirov <darkdomian@gmail.com>
2025-11-21 13:05:14 +0300

    refactor: just rename table where we get data

--- salesman-problem/ex01/team00_ex01.sql
+++ salesman-problem/ex01/team00_ex01.sql
@@ -1,8 +1,5 @@
-SELECT *
-FROM full_way
+SELECT * FROM possible_tours
 WHERE 
-    total_cost = (SELECT MIN(total_cost) FROM full_way)
-    OR total_cost = (SELECT MAX(total_cost) FROM full_way)
+    total_cost = (SELECT MIN(total_cost) FROM possible_tours)
+    OR total_cost = (SELECT MAX(total_cost) FROM possible_tours)
 ORDER BY total_cost, tour;
-
-DROP VIEW IF EXISTS full_way;
