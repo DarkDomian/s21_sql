c26822f441870bfe2a17055e2129e9ea3d4734fb
Demian trelawnm Domozhirov <darkdomian@gmail.com>
2025-11-25 16:31:23 +0300

    fix: small update to disable seq scan in ex04

--- database-index/ex04/day05_ex04.sql
+++ database-index/ex04/day05_ex04.sql
@@ -1,5 +1,7 @@
 CREATE UNIQUE INDEX idx_menu_unique ON menu (pizzeria_id, pizza_name);
 
+SET enable_seqscan = OFF;
+
 EXPLAIN ANALYZE
 SELECT * FROM menu
 WHERE pizzeria_id = (SELECT id FROM pizzeria WHERE name = 'DoDo Pizza')
