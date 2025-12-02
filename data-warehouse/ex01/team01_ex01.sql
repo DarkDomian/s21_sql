820ecdab6751a587465de2c3d4917907b380abcc
Selifanchik <selifanchik@mail.ru>
2025-12-02 03:35:09 +0300

    (team01): ex01 delete function, created single SQL query

--- data-warehouse/ex01/team01_ex01.sql
+++ data-warehouse/ex01/team01_ex01.sql
@@ -1,32 +1,27 @@
-drop function if exists fnc_rate;
-CREATE OR REPLACE FUNCTION fnc_rate(curr int, upt timestamp)
-RETURNS numeric
-LANGUAGE SQL
-AS $$
-    (
-        SELECT rate_to_usd
-        FROM currency
-        WHERE id = curr AND updated < upt
-        ORDER BY updated DESC
-        LIMIT 1
-    )
-    UNION ALL
-    (
-        SELECT rate_to_usd
-        FROM currency
-        WHERE id = curr AND updated > upt
-        ORDER BY updated
-        LIMIT 1
-    )
-    LIMIT 1;
-$$;
-
-SELECT 
+SELECT DISTINCT
     COALESCE("user".name, 'not defined') AS name, 
     COALESCE("user".lastname, 'not defined') AS lastname, 
-    temp_curr.name AS currency_name,
-    money * fnc_rate(currency_id, balance.updated) AS currency_in_usd
-FROM balance
-FULL JOIN "user" ON "user".id = balance.user_id
-JOIN (SELECT DISTINCT id, name FROM currency) AS temp_curr ON temp_curr.id = balance.currency_id
+    c.name AS currency_name,
+    -- temp_curr.name AS currency_name,
+    money * COALESCE
+    (
+        (
+            SELECT rate_to_usd
+            FROM currency
+            WHERE id = b.currency_id AND updated < b.updated
+            ORDER BY updated DESC
+            LIMIT 1
+        ),
+        (
+            SELECT rate_to_usd
+            FROM currency
+            WHERE id = b.currency_id AND updated > b.updated
+            ORDER BY updated
+            LIMIT 1
+        )
+    ) AS currency_in_usd
+FROM balance b
+FULL JOIN "user" ON "user".id = b.user_id
+-- JOIN (SELECT DISTINCT id, name FROM currency) as temp_curr ON temp_curr.id = b.currency_id
+JOIN currency c ON c.id = b.currency_id
 ORDER BY name DESC, lastname, currency_name;
