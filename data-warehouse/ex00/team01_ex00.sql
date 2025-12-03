67a0b39ce9183255228f06ef6b76af087874f3a1
Demian trelawnm Domozhirov <darkdomian@gmail.com>
2025-12-03 06:28:57 +0300

    feat(ex00): finish the first exercise

---
+++ data-warehouse/ex00/team01_ex00.sql
@@ -0,0 +1,27 @@
+WITH last_currency AS (
+	SELECT DISTINCT ON (id)
+		id,
+		name,
+		rate_to_usd,
+		updated
+    FROM currency
+    ORDER BY id, updated DESC
+),
+usr AS (
+	SELECT *
+	FROM "user"
+)
+
+SELECT 
+	COALESCE(ur.name, 'not defined') AS name,
+	COALESCE(ur.lastname, 'not defined') AS lastname,
+	bl.type,
+	SUM(bl.money) AS volume,
+	COALESCE(lc.name, 'not defined') AS currency_name,
+	COALESCE(lc.rate_to_usd, 1) AS last_rate_to_usd,
+	ROUND((SUM(bl.money) * COALESCE(lc.rate_to_usd, 1)), 0) AS total_volume_id_usd
+FROM usr ur
+FULL JOIN balance bl ON bl.user_id = ur.id
+FULL JOIN last_currency lc ON bl.currency_id = lc.id 
+GROUP BY ur.name, ur.lastname, bl.type, lc.name, lc.rate_to_usd
+ORDER BY 1 DESC, 2 , 3;
