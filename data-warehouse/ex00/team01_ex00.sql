6e9de96f56d20522f885f1731fa05fc4bb61ded8
Demian trelawnm Domozhirov <darkdomian@gmail.com>
2025-12-04 16:10:11 +0300

    refactor: some small change for readability

--- data-warehouse/ex00/team01_ex00.sql
+++ data-warehouse/ex00/team01_ex00.sql
@@ -6,10 +6,6 @@ WITH last_currency AS (
 		updated
     FROM currency
     ORDER BY id, updated DESC
-),
-usr AS (
-	SELECT *
-	FROM "user"
 )
 
 SELECT 
@@ -20,7 +16,7 @@ SELECT
 	COALESCE(lc.name, 'not defined') AS currency_name,
 	COALESCE(lc.rate_to_usd, 1) AS last_rate_to_usd,
 	ROUND((SUM(bl.money) * COALESCE(lc.rate_to_usd, 1)), 0) AS total_volume_id_usd
-FROM usr ur
+FROM "user" ur
 FULL JOIN balance bl ON bl.user_id = ur.id
 FULL JOIN last_currency lc ON bl.currency_id = lc.id 
 GROUP BY ur.name, ur.lastname, bl.type, lc.name, lc.rate_to_usd
--- data-warehouse/ex01/team01_ex01.sql
+++ data-warehouse/ex01/team01_ex01.sql
@@ -1,6 +1,9 @@
+insert into currency values (100, 'EUR', 0.85, '2022-01-01 13:29');
+insert into currency values (100, 'EUR', 0.79, '2022-01-08 13:29');
+
 SELECT DISTINCT
-    COALESCE("user".name, 'not defined') AS name, 
-    COALESCE("user".lastname, 'not defined') AS lastname, 
+    COALESCE(usr.name, 'not defined') AS name, 
+    COALESCE(usr.lastname, 'not defined') AS lastname, 
     c.name AS currency_name,
     money * COALESCE
     (
@@ -20,6 +23,6 @@ SELECT DISTINCT
         )
     ) AS currency_in_usd
 FROM balance b
-FULL JOIN "user" ON "user".id = b.user_id
+FULL JOIN "user" usr ON usr.id = b.user_id
 JOIN currency c ON c.id = b.currency_id
 ORDER BY name DESC, lastname, currency_name;
